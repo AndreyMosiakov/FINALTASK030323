25ca252201b0e89401885f77c3307d230b85f5eb
AndreyMosiakov <mosikoff@yandex.ru>
2023-03-05 16:12:16 +0300

    написали программу

--- Program.cs
+++ Program.cs
@@ -20,8 +20,26 @@ int GetCount(string[] array)
         if (array[i].Length <= 3) count++;
     }
     return count;
-}  
-
-
-
+}
+string[] GetNewArray(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[newArray.Length - count] = array[i];
+            count--;
+        }
+    }
+    return newArray;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
 int count = GetCount(array);
+string[] newArray = GetNewArray(array, count);
+PrintArray(newArray);
